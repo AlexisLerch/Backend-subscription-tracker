0db09d4881f0235fdea0397fbdad04aad390121a
Alexis Lerch <alexis.lerch123@gmail.com>
2025-04-15 16:13:28 -0300

    Implementando createSubscription y getUserSubscription

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -10,9 +12,7 @@ subscriptionRouter.get("/:id", (req, res) => {
   res.send({ title: "GET subscription details" });
 });
 
-subscriptionRouter.post("/", (req, res) => {
-  res.send({ title: "CREATE new subscription" });
-});
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) => {
   res.send({ title: "UPDATE subscription" });
@@ -26,9 +26,7 @@ subscriptionRouter.get('user/:id', (req, res) => {
   res.send({ title: "GET all subscriptions for user" });
 });
 
-subscriptionRouter.get('/:id/cancel', (req, res) => {
-  res.send({ title: "CANCEL subscription" });
-});
+subscriptionRouter.get('/:id/cancel', authorize, getUserSubscriptions);
 
 subscriptionRouter.get('/upcoming-renewals', (req, res) => {
   res.send({ title: "GET upcoming renewals" });
